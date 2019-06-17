8120ee45f8f337687020b2f7612ecdc8c47816fc
Jamie Pinheiro <jamiepinheiro@gmail.com>
2019-06-17 17:08:15 -0400

    Setup basic game board

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<Game :player1="'Player 1'" :player2="'Player 2'"/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Game from './components/Game.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	components: {
+		Game
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/Game.vue
@@ -0,0 +1,62 @@
+<template>
+	<div>
+		<h1>Game</h1>
+		<h4 class="mb-3">{{player1}} vs {{player2}}</h4>
+		<table class="col-md-4 col-sm-10 mx-auto">
+			<tr>
+				<td><div class="content">1</div></td>
+				<td><div class="content">1</div></td>
+				<td><div class="content">1</div></td>
+			</tr>
+			<tr>
+				<td><div class="content">1</div></td>
+				<td><div class="content">1</div></td>
+				<td><div class="content">1</div></td>
+			</tr>
+			<tr>
+				<td><div class="content">1</div></td>
+				<td><div class="content">1</div></td>
+				<td><div class="content">1</div></td>
+			</tr>
+		</table>
+	</div>
+
+</template>
+
+<script>
+
+export default {
+	props: ["player1", "player2"],
+	methods: {
+		a() {
+			console.log(1);
+			
+		}
+	}
+}
+</script>
+
+<style>
+	table {
+		width: 90%;
+	}
+	td {
+		width: 33.33%;
+		position: relative;
+		border: black;
+		border-style: solid;
+	}
+	td:after {
+		content: '';
+		display: block;
+		margin-top: 100%;
+	}
+	td .content {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background-image: url("../assets/donut.svg");
+	}
+</style>
